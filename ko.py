1107656a88727ec4960b01fb0bdaec16be8c09fb
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-04-14 22:38:02 -0600

    delete files to replace for machine learning code

--- ko.py
+++
@@ -1,9 +0,0 @@
-from zipline.examples import buyapple
-from zipline.api import order, record, symbol
-
-def initialize(context):
-    pass
-
-def handle_data(context, data):
-    order(symbol("AAPL"), 10)
-    record(APPL=data.current(symbol("AAPL"), price))
